0fd1cf4bf21927457cab2fc01de8b23cccf0c3a1
AClon314 <30747832+AClon314@users.noreply.github.com>
2025-09-02 07:30:46 +0800

    feat(mirror_cn): 优化本地代理服务功能
    - 添加文件重定向和 URL 重定向功能
    - 重构代码，增加更多功能和灵活性
    - 优化日志记录和错误处理
    - 支持命令行参数解析
    - 新增本地文件为空时的处理逻辑

--- src/mirror_cn/redirect.py
+++ src/mirror_cn/redirect.py
@@ -1,48 +1,94 @@
-#!/bin/env mitmdump -s redirect.py -p 8080 --mode upstream:http://localhost:10808
-'''
+#!/bin/env mitmdump -s redirect.py -p 3378
+"""
+mitmweb -s redirect.py -p 3378 --mode local --mode upstream:http://localhost:10808
 TODO: currently you need to install manually mitmproxy .pem certificate into system trust store
-'''
-import os
-from mitmproxy import http, ctx
-from mitmproxy.http import Response
-Log = ctx.log
+"""
+from pathlib import Path
+
+PORT = 3378
+PROG = "mitmweb"
+ARGS = ["-s", __file__, "-p", str(PORT)]
 REMAP = {
-    'https://github.com/actions/python-versions/releases/download/3.12.11-15433310049/python-3.12.11-linux-22.04-x64.tar.gz': '/home/n/download/python-3.12.11-linux-22.04-x64.tar.gz'
+    f"http://127.0.0.1:{PORT}/builds/chromium/1187/chromium-win64.zip": Path(
+        r"D:\Downloads\chromium-win64.zip"
+    ),
+    f"http://127.0.0.1:{PORT}/builds/chromium/1187/chromium-headless-shell-win64.zip": Path(
+        r"D:\Downloads\chromium-headless-shell-win64.zip"
+    ),
 }
+import os
+import logging
+from mitmproxy import http
+from mitmproxy.http import Response
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s %(asctime)s %(filename)s:%(lineno)d\t%(message)s",
+)
+Log = logging.getLogger(__name__)
+BLANK_RESPONSE_HEADER = {"Content-Type": "text/plain"}
 
 
 def request(flow: http.HTTPFlow) -> None:
-    file = REMAP.get(flow.request.url, None)
-    if file:
-        Log.info(f"[Remap 🌐 →📄]\t{flow.request.url=}")
-        try:
-            # 读取本地文件内容
-            with open(file, 'rb') as f:
-                content = f.read()
-            if len(content) == 0:
-                Log.warn(f"Local {file=} is empty.")
-                # 可选操作：返回空响应
-                flow.response = Response.make(
-                    204,  # 无内容
-                    b"",
-                    {"Content-Type": "text/plain"}
-                )
-                return
-
-            # 构建响应
-            flow.response = Response.make(
-                200,  # 状态码
-                content,  # 响应内容
-                {"Content-Type": "application/octet-stream"}  # 响应头
-            )
-            Log.info(f"✅ {file=}")
-        except Exception as e:
-            Log.warn(e)
-            # 可选操作：返回错误响应
-            flow.response = Response.make(
-                500,  # 内部服务器错误
-                b"Failed to load local file",
-                {"Content-Type": "text/plain"}
-            )
+    src = flow.request.url
+    dst = REMAP.get(src, None)
+    if isinstance(dst, Path):
+        return redir_file(flow, dst)
     else:
-        Log.info(f"[Direct]\t{flow.request.url=}")
+        return redir_url(flow)
+
+
+def redir_url(flow: http.HTTPFlow):
+    src = flow.request.url
+    if (
+        src.startswith("http://mirror-pypi.cn/raw-proxy-tsinghua-cargo")
+        and flow.request.method == "HEAD"
+    ):
+        flow.request.method = "GET"
+        Log.info(f"[Method Change] HEAD → GET for {flow.request.url}")
+        return
+    Log.info(f"[Direct]\t{flow.request.method}\t{src=}")
+
+
+def redir_file(flow: http.HTTPFlow, file: Path):
+    Log.info(f"[Remap 🌐 →📄]\t{flow.request.url=}")
+    if file.exists():
+        MSG = f"FileNotFoundError: {file=}"
+        Log.warning(MSG)
+        flow.response = Response.make(404, MSG, BLANK_RESPONSE_HEADER)
+        return
+    try:
+        with open(file, "rb") as f:
+            content = f.read()
+    except Exception as e:
+        Log.error("", exc_info=e)
+        flow.response = Response.make(500, f"FileException: {e}", BLANK_RESPONSE_HEADER)
+    if len(content) == 0:
+        Log.warning(f"{file=} is empty.")
+        flow.response = Response.make(204, b"", BLANK_RESPONSE_HEADER)
+        return
+    flow.response = Response.make(200, content, BLANK_RESPONSE_HEADER)
+    Log.info(f"[File]\t{file=}")
+
+
+def parse():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="适用于离线安装,对于硬编码的程序极为有效"
+    )
+    parser.add_argument("--verbose", action="store_true")
+    args = parser.parse_args()
+    Log.setLevel(logging.DEBUG) if args.verbose else None
+    return parser.parse_known_args()
+
+
+def main():
+    ns, args = parse()
+    args += ARGS
+    Log.debug(f"{locals()=}")
+    os.execlp(PROG, PROG, *args)
+
+
+if __name__ == "__main__":
+    main()
